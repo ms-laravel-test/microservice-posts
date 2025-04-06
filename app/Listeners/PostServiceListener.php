6822047f2f2f9ef47042c923697e4683eb18f1a5
Ali Kazemayni <116560378+aliKazemayni@users.noreply.github.com>
2025-04-06 13:16:00 +0330

    feat(rabbitmq): use listen method in post listener

--- app/Listeners/PostServiceListener.php
+++ app/Listeners/PostServiceListener.php
@@ -2,45 +2,21 @@
 
 namespace App\Listeners;
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
+use App\Facade\RabbitMq;
 use App\Models\Post;
+use Exception;
 
 class PostServiceListener
 {
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function listen(): void
     {
-        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
-        $channel = $connection->channel();
-
-        $channel->queue_declare('get_user_posts', false, true, false, false);
-
-        echo " [*] Waiting for messages. To exit press CTRL+C\n";
-
-        $callback = function ($msg) use ($channel) {
-            $data = json_decode($msg->body, true);
+        echo "start listen post :\n";
+        RabbitMq::listen('get_user_posts' , function ($data) {
             $userId = $data['user_id'];
-
-            $posts = Post::query()->where('user_id', $userId)->get();
-            $response = json_encode($posts);
-
-            $responseMsg = new AMQPMessage($response, [
-                'correlation_id' => $msg->get('correlation_id'),
-            ]);
-
-            $channel->basic_publish($responseMsg, '', $msg->get('reply_to'));
-        };
-
-        $channel->basic_consume('get_user_posts', '', false, true, false, false, $callback);
-
-        while ($channel->is_consuming()) {
-            $channel->wait();
-        }
-
-        $channel->close();
-        $connection->close();
+            return Post::query()->where('user_id', $userId)->get();
+        });
     }
 }
